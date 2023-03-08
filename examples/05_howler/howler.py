4d29912aebc1b9869f10749306bba7289e53c989
wildcatag <mylesd@arizona.edu>
2023-03-08 16:01:49 -0700

    update 2

--- examples/05_howler/howler.py
+++ examples/05_howler/howler.py
@@ -18,6 +18,7 @@ def get_args():
 
     parser.add_argument('text',
                         metavar='str',
+                        type=str,
                         help='Input text')
 
     parser.add_argument('-o',
@@ -25,7 +26,7 @@ def get_args():
                         help='Output filename',
                         metavar='str',
                         type=str,
-                        default='None')
+                        default='')
 
     args = parser.parse_args()
 
@@ -39,11 +40,24 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
+<<<<<<< HEAD
     
     if args.outfile:
         print(args.text.upper(), file=open(args.outfile, 'wt'))
+=======
+
+    # you named this outfile in the argparse above, not out
+    # changed to outfile
+    if args.outfile:
+        # you need to open the file handle first to print to it
+        out_fh = open(args.outfile, 'wt')
+        # I added the '\n' because you strip this off above
+        # I prefer to use write like so, as we do in the chapter
+        # out_fh.write(args.text.upper() + '\n') 
+        print(args.text.upper() + '\n', file=out_fh)
+>>>>>>> d6bc64c128a48767eb46d46f919fed276df74aa3
     else:
-        print(args.text.upper())
+        print(args.text.upper() + '\n')
    
 # --------------------------------------------------
 if __name__ == '__main__':
