7c61e0d84613855bd14cc5df66276650e67d46be
wildcatag <mylesd@arizona.edu>
2023-04-23 13:45:53 -0700

    Stuck on grep

--- assignments/12_grep/grep.py
+++ assignments/12_grep/grep.py
@@ -12,7 +12,7 @@ import argparse
 #import io
 import os
 #import random
-#import re
+import re
 #import string
 import sys
 #from pprint import pprint
@@ -51,19 +51,15 @@ def get_args():
 
     args = parser.parse_args()
     
-    #if os.path.isfile(args.file) == False:
-    ##    parser.error(f'--col "{args.file}" No such file or directory:')
-    #else:
-    #    if os.path.isfile(args.pattern) == False:
-     #       parser.error(f'--col "{args.FILE2}" No such file or directory:')
+    fh = re.search("txt\b", args.file)
+    #fh = os.path.isfile(args.file)
+    patternhandle = str(args.pattern)
 
-    #if args.pattern != str:
-    #    parser.error(f'--seqtype "{args.seqtype}" must be a valid sequence')
-
-    #if not 0 < args.pctgc < 1:
-     #   parser.error(f'--pctgc "{args.pctgc}" must be between 0 and 1')
-
-
+    if patternhandle:
+        if not fh:
+        #if not args.file:
+        #if os.path.isfile(args.file) == False:
+            parser.error(f'--col "{args.file}" No such file or directory:')
 
     return args
 
@@ -72,8 +68,41 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
+    
+    #search_var = args.pattern
+    new_list = []
+    idx = 1
+    temp_line = ""
+    
+    if args.insenstive:
+        for line in args.file:    
+            #if temp_line.get(args.pattern.upper()) and temp_line.get(args.pattern.lower()) is None:
+            if re.IGNORECASE(re.search(args.pattern)) == True:
+                new_list = ""
+                print (new_list)
+            else:
+                new_list = new_list.insert(idx, temp_line)
 
-
+    else:
+        for line in args.file:
+            temp = line.rstrip().split()
+            temp_search = re.search(args.pattern, temp)#if args.pattern in temp_line: 
+            if temp_search:
+        #if re.search(args.pattern, temp) == True:
+                new_list.insert(idx, line)
+        
+    if args.outfile: #if flag, write each line
+        #args.outfile.write(new_list)
+        args.outfile.write(f'"{args.file.name}" {new_list}''\n')
+        print(args.outfile)
+         #   print(f'"{args.outfile.name}": {TEXT RETURNED} "\n"')
+        #else:
+         #   print(f'"{args.outfile.name}": {TEXT RETURNED} "\n"')
+    else:
+        print(f'"{args.file.name}" {new_list}')
+    
+    #The output should include the name of the file when there is more than one file argument
+    #print(f'"{args.outfile.name}": {TEXT RETURNED} "\n"')
 
 # --------------------------------------------------
 if __name__ == '__main__':
