e5abb64acafbdd72a6261ab02e38f32180085987
wildcatag <mylesd@arizona.edu>
2023-05-03 11:28:21 -0700

    common progress

--- assignments/10_common/common.py
+++ assignments/10_common/common.py
@@ -9,10 +9,10 @@ Purpose: Find common words
 import argparse
 #import csv
 #import emoji
-import io
+#import io
 #import os
 #import random
-import re
+#import re
 #import string
 import sys
 #from pprint import pprint
@@ -49,58 +49,65 @@ def get_args():
                     default= sys.stdout)
 
     args = parser.parse_args()
-
-    #The program should halt with an error message 
-    # if either of the positional arguments are not 
+    #The program should halt with an error message if either of the positional arguments are not 
     # the names of valid, readable files
-
-
     return args
 
 # --------------------------------------------------
 def main():
     """Make a jazz noise here"""
-
+    #The program should find sequences are shared & The program should find common words
     args = get_args()
-
-    #The program should find sequences are shared
-    #The program should find common words
     
+    fh1 = args.file1 #file handle opens -> need to read it now
+    fh2 = args.file2
+    
+    fhread1 = fh1.read() #read the open files and intialize a variable to do it
+    fhread2 = fh2.read()
+    fho = args.outfile
     comp_set1 = set() #comparative set 1
     comp_set2 = set() #comparative set 2
     compoutfile = set() #file to write values to
     
     #open each file, read the line, split to words to list, 
     # assign list to set
-    
-    for fh1 in args.file1:
-        for line1 in fh1:
-            for word1 in line1.split():
-            #for word1 in line1.splitlines().split()
-            #for word1 in line1.split(re.sub('[^a-zA-Z]', '', word1)):
-                comp_set1.add(word1)
-                #comp_set1 = comp_set1.add(word1) --NO
+    ####
+    xxx = ""
+    #for fh1 in args.file1:
+    for line1 in fhread1:
+        
+        #for word1 in line1:
+        #for word1 in line1.splitlines().split()
+        #for word1 in line1.split(re.sub('[^a-zA-Z]', '', word1)):
+        comp_set1.add(fhread1)
+        print(comp_set1)    #printfriendly = ", ".join(compoutfile)
+            #comp_set1 = comp_set1.add(word1) --NO
                     
     #open each file, read the line, split to words, assign to set2
-    for fh2 in args.file2:
-        for line2 in fh2:        
-            for word2 in line2.split():
-                comp_set2.add(word2)
+    #for fh2 in args.file2:
+    for line2 in fhread2:        
+        #for word2 in line2:
+        comp_set2.add(fhread2)
                 #comp_set2 = comp_set2.add(word2) --NO
             #for word2 in line2.split(re.sub('[^a-zA-Z]', '', word2)):
             #   #re.sub('[^a-zA-Z]', '', word2)
     #compare sets and write commons to a outfile
     
     compoutfile = comp_set1.intersection(comp_set2)
+    printfriendly = ", ".join(compoutfile)
+    
+    print(compoutfile)
+    print(printfriendly)
+
 
        #If the "-o|--outfile" option is provided, 
     # then the program should print the words to the 
     # provided output file and nothing to STDOUT: 
     # #if outfile flag is true, write contents to file named, else stdout
-    if args.outfile:
-        args.outfile = compoutfile.write()
-    
-    print(compoutfile)
+    #if fho:
+    #    fho.write(compoutfile)
+    #else:
+    #    print(compoutfile)
     #print(comp_set1)
     #print(comp_set2)
     
